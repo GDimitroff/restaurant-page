e1a9cc90903f575ef186b3bfe3e26432cf596444
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-06-06 18:26:23 +0300

    Install webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("It's working");
+import print from './print';
+
+function component() {
+  print();
+
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  p.textContent = "It's working!";
+
+  div.appendChild(p);
+  div.classList.add('red');
+
+  return div;
+}
+
+document.body.appendChild(component());
---
+++ src/print.js
@@ -0,0 +1,5 @@
+const print = () => {
+  console.log('Working!');
+};
+
+export default print;
