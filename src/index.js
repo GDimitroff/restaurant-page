1a3e9897fbcbec854a7c183882c2711726d1a20b
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-06-09 15:46:17 +0300

    Add home page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './main.css';
 import createHeader from './components/header';
 import createNavbar from './components/navbar';
 import createFooter from './components/footer';
-import createHome from './modules/home';
+import loadHome from './modules/home';
 
 function init() {
   const body = document.querySelector('body');
@@ -13,12 +13,12 @@ function init() {
   header.appendChild(navbar);
 
   const main = document.createElement('main');
-  const homePage = createHome();
-  main.appendChild(homePage);
 
   body.appendChild(header);
   body.appendChild(main);
   body.appendChild(footer);
+
+  loadHome();
 }
 
 init();
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,8 +1,29 @@
 function createHome() {
-  const div = document.createElement('div');
-  div.textContent = 'Home content';
+  const section = document.createElement('div');
+  section.classList.add('home');
 
-  return div;
+  const p = document.createElement('p');
+  p.textContent = 'Welcome';
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Passion';
+
+  const hr = document.createElement('hr');
+
+  const pDescription = document.createElement('p');
+  pDescription.textContent = 'Taste the passion and forget about everything!';
+
+  const button = document.createElement('button');
+  button.textContent = 'Menu';
+
+  section.append(p, h1, hr, pDescription, button);
+
+  return section;
 }
 
-export default createHome;
+function loadHome() {
+  const main = document.querySelector('main');
+  const home = createHome();
+  main.appendChild(home);
+}
+
+export default loadHome;
